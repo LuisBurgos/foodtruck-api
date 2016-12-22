2f26ada111208b2d8fe3d3730f43edaf15fb04b6
Luis Burgos <luis.burgos.1995@gmail.com>
2016-12-22 01:57:28 -0600

    Add methods to register, login and logout

--- src/controller/account.js
+++ src/controller/account.js
@@ -11,5 +11,39 @@ import { generateAccessToken, respond, authenticate } from '../middleware/authmi
 export default({ config, db }) => {
   let api = Router()
 
+  // '/v1/account'
+  api.post('/register', (req, res) => {
+    Account.register(new Account({
+      username: req.body.email
+    }), req.body.password, function(err, account){
+      if(err){
+        res.send(err)
+      } else {
+        passport.authenticate('local', {
+          session: false //We are going to use clients not sessions
+        })(req, res, () => {
+          res.status(200).send('Sucessfully created new account')
+        })
+      }
+    })
+  })
+
+  // '/v1/account/login'
+  api.post('/login', passport.authenticate('local', {
+    session: false,
+    scope: []
+  }), generateAccessToken, respond)
+
+  // '/v1/account/logout'
+  api.get('/logout', authenticate, (req, res) => {
+    res.logout()
+    res.status(200).send('Sucessfully logged out')
+  })
+
+  // '/v1/account/me'
+  api.get('/me', authenticate, (req, res) => {
+    res.status(200).json(req.user)
+  })
+
   return api
 }
